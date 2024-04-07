db549d0525b2075b0649f27fdfd7c96770e2fe86
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2024-04-07 19:13:54 +0200

    model transforms

--- webgpu/genka/camera.ts
+++ webgpu/genka/camera.ts
@@ -4,6 +4,7 @@ import type { UBI } from '../pipeline'
 
 export function useCamera(gpu: UBI) {
     const viewProjection = getViewProjectionMatrix()
+    const mvpMatrix = modelMatrix(viewProjection)
 
     const matrixSize = 4 * 16 // 4x4 matrix
     const offset = 256 // uniformBindGroup offset must be 256-byte aligned
@@ -16,69 +17,46 @@ export function useCamera(gpu: UBI) {
             gpu.device.queue.writeBuffer(
                 buffer,
                 0,
-                viewProjection.buffer,
+                mvpMatrix.buffer,
                 0,
-                viewProjection.byteLength
+                mvpMatrix.byteLength
             )
         },
     })
 }
 
-export class Camera {
-    public uniform(device) {
-        getViewProjectionMatrix()
+function modelMatrix(cameraProjectionMatrix: mat4) {
+    const position = vec3.fromValues(0, 0, 0)
+    const rotation = vec3.fromValues(0, 0, 0)
+    const scale = vec3.fromValues(1, 1, 1)
 
-        const matrixSize = 4 * 16 // 4x4 matrix
-        const offset = 256 // uniformBindGroup offset must be 256-byte aligned
-        const uniformBufferSize = offset + matrixSize
-
-        return uniformBuffer(
-            { device },
-            {
-                label: 'Camera View/Projection Matrix Buffer',
-                size: uniformBufferSize,
-                update: (buffer) => {
-                    const sourceArray = getViewProjectionMatrix()
-                    device.queue.writeBuffer(
-                        buffer,
-                        0,
-                        sourceArray.buffer,
-                        0,
-                        sourceArray.byteLength
-                    )
-                },
-            }
-        )
-    }
-}
-
-function updateTransformationMatrix(cameraProjectionMatrix: mat4) {
-    // MOVE / TRANSLATE OBJECT
     const modelMatrix = mat4.create()
-    mat4.translate(
-        modelMatrix,
-        modelMatrix,
-        vec3.fromValues(this.x, this.y, this.z)
-    )
-    mat4.rotateX(modelMatrix, modelMatrix, this.rotX)
-    mat4.rotateY(modelMatrix, modelMatrix, this.rotY)
-    mat4.rotateZ(modelMatrix, modelMatrix, this.rotZ)
+
+    // TRANSLATE
+    mat4.translate(modelMatrix, modelMatrix, position)
+
+    // ROTATE
+    mat4.rotateX(modelMatrix, modelMatrix, rotation[0])
+    mat4.rotateY(modelMatrix, modelMatrix, rotation[1])
+    mat4.rotateZ(modelMatrix, modelMatrix, rotation[2])
+
+    // SCALE
+    mat4.scale(modelMatrix, modelMatrix, scale)
+
+    const mvpMatrix = mat4.create()
 
     // PROJECT ON CAMERA
-    mat4.multiply(
-        this.modelViewProjectionMatrix,
-        cameraProjectionMatrix,
-        modelMatrix
-    )
+    mat4.multiply(mvpMatrix, cameraProjectionMatrix, modelMatrix)
+    return mvpMatrix as Float32Array
 }
 
 function getViewProjectionMatrix() {
     const position = vec3.fromValues(5, 5, 5)
     const target = vec3.fromValues(0, 0, 0)
     const up = vec3.fromValues(0, 1, 0)
-    const fov = Math.PI / 4
+    const fov = Math.PI / 4 // maybe 2 instead
     const aspect = 512 / 512
-    const near = 0
+    const near = 1
     const far = 1000.0
 
     const viewMatrix = mat4.create()
@@ -89,6 +67,5 @@ function getViewProjectionMatrix() {
 
     const viewProjectionMatrix = mat4.create()
     mat4.multiply(viewProjectionMatrix, projectionMatrix, viewMatrix)
-
     return viewProjectionMatrix as Float32Array
 }
